5ce774c692a04aaa5541ab815ec671361a6bf49e
s205347 <s205347@student.dtu.dk>
2021-11-28 23:00:41 +0100

    Lavet nye fragments

---
+++ app/src/main/java/com/example/individualassignment_wheeloffortune/FirstFragment.kt
@@ -0,0 +1,26 @@
+package com.example.individualassignment_wheeloffortune
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.view.*
+
+class FirstFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
+
+        view.textView1.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.navigateToSecondFragment)}
+
+        return view
+
+    }
+}
+
---
+++ app/src/main/java/com/example/individualassignment_wheeloffortune/SecondFragment.kt
@@ -0,0 +1,25 @@
+package com.example.individualassignment_wheeloffortune
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+import kotlinx.android.synthetic.main.fragment_first.view.*
+import kotlinx.android.synthetic.main.fragment_second.view.*
+
+class SecondFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_second, container, false)
+
+        view.textView2.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.navigateToFirstFragment)}
+
+        return view
+    }
+}
