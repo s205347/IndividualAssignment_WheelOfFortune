83a5833ccad3fb2aff26bd35bebe653ced84bf6f
s205347 <s205347@student.dtu.dk>
2021-11-28 17:56:06 +0100

    Clean up

--- app/src/main/java/com/example/individualassignment_wheeloffortune/MainActivity.kt
+++ app/src/main/java/com/example/individualassignment_wheeloffortune/MainActivity.kt
@@ -8,15 +8,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val startscreenFragment =
-        val mainscreenFragmentFragment = MainscreenFragment()
-
-        supportFragmentManager.beginTransaction().apply {
-            replace(R.id.mainscreenFragment, fra)
-        }
-
-
-
     }
 }
