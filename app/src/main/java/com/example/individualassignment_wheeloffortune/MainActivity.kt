d0d98c268acb67fb674a622b8b2e3b454be984e9
s205347 <s205347@student.dtu.dk>
2021-11-29 17:42:59 +0100

    tilføjet kode

--- app/src/main/java/com/example/individualassignment_wheeloffortune/MainActivity.kt
+++ app/src/main/java/com/example/individualassignment_wheeloffortune/MainActivity.kt
@@ -5,8 +5,38 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    var score = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        var hemmeligSætning: String = "wheel of fortune "
+//        Scanner scan
+//        scan = new Scanner(System.in)
+//        var spillerGæt: String = " "
+//        var ikkeFærdig = true
+//
+//        while (score !=-1) {
+//            ikkeFærdig = false;
+//            for(char hemmeligtBogstav : hemmeligSætning.toCharArray()) {
+//                if (spillerGæt.indexOf(hemmeligtBogstav) == -1) {
+//                    System.out.print("*");
+//                    ikkeFærdig = true;
+//                } else {
+//                    System.out.print(hemmeligtBogstav);
+//                }
+//
+//            }
+//            System.out.println("\nIndtast venligt det bogstav du gætter på");
+//            val bogstav = scan.next();
+//            spillerGæt += bogstav;
+//        }
     }
+
+
+
 }
+
+
