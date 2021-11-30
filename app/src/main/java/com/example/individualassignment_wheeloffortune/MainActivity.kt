f843adcd08dfcb67a1ce3447ae2efae9c9a3cd4e
s205347 <s205347@student.dtu.dk>
2021-11-30 01:28:55 +0100

    ændring i pointericon1.png navngivning

--- app/src/main/java/com/example/individualassignment_wheeloffortune/MainActivity.kt
+++ app/src/main/java/com/example/individualassignment_wheeloffortune/MainActivity.kt
@@ -6,32 +6,10 @@ import android.widget.ImageView
 
 class MainActivity : AppCompatActivity() {
 
-    var score = 0
-    val lives = 6
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-//        val hemmeligSætning = "wheel of fortune "
-//        val scan: Scanner
-//        scan = Scanner(System.`in`)
-//        var spillerGæt = " "
-//
-//        while (lives > 0) {
-//            for (hemmeligtBogstav in hemmeligSætning.toCharArray()) {
-//                if (spillerGæt.indexOf(hemmeligtBogstav) == -1) {
-//                    print("*")
-//                    lives-1
-//                } else {
-//                    print(hemmeligtBogstav)
-//                }
-//            }
-//            println("\nIndtast venligt det bogstav du gætter på")
-//            val bogstav = scan.next()
-//            spillerGæt += bogstav
-//        }
-//        println("Tillykke du er færdig og har vundet spillet! ;)")
     }
 
 
--- app/src/main/java/com/example/individualassignment_wheeloffortune/SecondFragment.kt
+++ app/src/main/java/com/example/individualassignment_wheeloffortune/SecondFragment.kt
@@ -10,8 +10,12 @@ import android.widget.ImageView
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_first.view.*
 import kotlinx.android.synthetic.main.fragment_second.view.*
+import java.util.*
 
 class SecondFragment : Fragment() {
+
+    var score = 0
+    val lives = 6
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,5 +27,28 @@ class SecondFragment : Fragment() {
         view.textView2.setOnClickListener{ Navigation.findNavController(view).navigate(R.id.navigateToThirdFragment)}
 
         return view
+
+//        val hemmeligSætning = "wheel of fortune "
+//        val scan: Scanner
+//        scan = Scanner(System.`in`)
+//        var spillerGæt = " "
+//
+//        while (lives > 0) {
+//            for (hemmeligtBogstav in hemmeligSætning.toCharArray()) {
+//                if (spillerGæt.indexOf(hemmeligtBogstav) == -1) {
+//                    print("*")
+//                    lives-1
+//                } else {
+//                    print(hemmeligtBogstav)
+//                }
+//            }
+//            println("\nIndtast venligt det bogstav du gætter på")
+//            val bogstav = scan.next()
+//            spillerGæt += bogstav
+//        }
+//        println("Tillykke du er færdig og har vundet spillet! ;)")
     }
+
+
+
 }
