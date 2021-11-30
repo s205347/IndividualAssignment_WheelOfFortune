23066a68733bb465567e4d0d4acb2904deb630ef
s205347 <s205347@student.dtu.dk>
2021-12-01 00:19:19 +0100

    Lavet ny klasse Sentences, som indeholder de forskellige hemmeliges ætninger

---
+++ app/src/main/java/com/example/individualassignment_wheeloffortune/Sentences.kt
@@ -0,0 +1,23 @@
+package com.example.individualassignment_wheeloffortune
+
+class Sentences {
+    val secretSentences = listOf<String>(
+        "Game/of/Thrones",
+        "Breaking/Bad",
+        "Orange/is/the/new/black",
+        "Stranger/things",
+        "The/walking/dead",
+        "Greys/anatomy",
+        "The/flash",
+        "La/casa/de/papel",
+        "The/Bing/Bang/Theory",
+        "Prison/Break",
+        "The/Simpsons",
+        "Rick/and/Morty",
+        "Friends",
+        "Narcos",
+        "How/I/met/your/mother",
+        "Modern/family",
+        "House/of/cards"
+    )
+}
