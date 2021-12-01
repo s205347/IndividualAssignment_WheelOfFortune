1cf8f5bd1e1df3a43aae6b466506c037df4256ca
s205347 <s205347@student.dtu.dk>
2021-12-02 00:19:39 +0100

    Videreudviklet på spillogikken og fjernet 2 indtil videre ubrugelige fragments

--- app/src/main/java/com/example/individualassignment_wheeloffortune/MainActivity.kt
+++ app/src/main/java/com/example/individualassignment_wheeloffortune/MainActivity.kt
@@ -2,18 +2,87 @@ package com.example.individualassignment_wheeloffortune
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.children
+import kotlinx.android.synthetic.main.fragment_second.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val gameControl = GameControl
+    private lateinit var lettersLayout: ConstraintLayout
+    private lateinit var newGameButton: Button
+    private lateinit var imageView: ImageView
+    private lateinit var lettersUsedTextView: TextView
+    private lateinit var wordTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        imageView = imageView.findViewById(R.id.imageView)
+        wordTextView = imageView.findViewById(R.id.wordTextView)
+        lettersUsedTextView = imageView.findViewById(R.id.lettersUsedTextView)
+        lettersLayout = imageView.findViewById(R.id.lettersLayout)
+
+        newGameButton.setOnClickListener {
+            startNewGame()
+
+        }
+        val gameState = gameControl.startNewGame()
+        updateUI(gameState)
+
+        lettersLayout.children.forEach { letterView ->
+            if (letterView is TextView) {
+                letterView.setOnClickListener {
+                    val gameState = gameControl.play((letterView).text[0])
+                    updateUI(gameState)
+                    letterView.visibility = View.GONE
+                }
+            }
+        }
     }
 
 
+    private fun updateUI(gameCondition: GameCondition) {
+        when (gameCondition) {
+            is GameCondition.Lost -> showGameLost(gameCondition.sentenceReveal)
+            is GameCondition.Undergoing -> {
+                wordTextView.text = gameCondition.secretSentence
+                lettersUsedTextView.text = "Letters used: ${gameCondition.lettersAlreadyUsed}"
+            }
+            is GameCondition.Won -> showGameWon(gameCondition.sentenceReveal)
+        }
+    }
 
+    private fun showGameLost(wordToGuess: String) {
+        wordTextView.text = wordToGuess
+        gameLostTextView.visibility = View.VISIBLE
+        lettersLayout.visibility = View.GONE
+    }
+
+    private fun showGameWon(wordToGuess: String) {
+        wordTextView.text = wordToGuess
+        gameWonTextView.visibility = View.VISIBLE
+        lettersLayout.visibility = View.GONE
+    }
+
+    private fun startNewGame() {
+        gameLostTextView.visibility = View.GONE
+        gameWonTextView.visibility = View.GONE
+        val gameCondition = gameControl.startNewGame()
+        lettersLayout.visibility = View.VISIBLE
+        lettersLayout.children.forEach { letterView ->
+            letterView.visibility = View.VISIBLE
+        }
+        updateUI(gameCondition)
+    }
 }
+
+
+
+
 
 
--- app/src/main/java/com/example/individualassignment_wheeloffortune/SecondFragment.kt
+++ app/src/main/java/com/example/individualassignment_wheeloffortune/SecondFragment.kt
@@ -9,89 +9,25 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 import androidx.core.view.children
 import androidx.navigation.Navigation
 import kotlinx.android.synthetic.main.fragment_first.view.*
+import kotlinx.android.synthetic.main.fragment_second.*
 import kotlinx.android.synthetic.main.fragment_second.view.*
 import java.util.*
 
 class SecondFragment : Fragment() {
 
-    private val gameControl = GameControl
-    private lateinit var lettersLayout: ConstraintLayout
-    private lateinit var newGameButton: Button
-    private lateinit var imageView: ImageView
-    private lateinit var lettersUsedTextView: TextView
-    private lateinit var wordTextView: TextView
 
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_first, container, false)
 
-    override fun onCreateView(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.fragment_second)
-        imageView = imageView.findViewById(R.id.imageView)
-        wordTextView = imageView.findViewById(R.id.wordTextView)
-        lettersUsedTextView = imageView.findViewById(R.id.lettersUsedTextView)
-        lettersLayout = imageView.findViewById(R.id.lettersLayout)
-        newGameButton.setOnClickListener {
-            startNewGame()
-
-        }
-        val gameState = gameControl.startNewGame()
-        updateUI(gameState)
-
-        lettersLayout.children.forEach { letterView ->
-            if (letterView is TextView) {
-                letterView.setOnClickListener {
-                    val gameState = gameControl.play((letterView).text[0])
-                    updateUI(gameState)
-                    letterView.visibility = View.GONE
-                }
-            }
-        }
+        return view
     }
-
-
-
-    private fun updateUI(gameCondition: GameCondition) {
-        when (gameCondition) {
-            is GameCondition.Lost -> showGameLost(gameCondition.)
-            is GameCondition.Undergoing -> {
-                wordTextView.text = gameCondition.secretSentence
-                lettersUsedTextView.text = "Letters used: ${gameCondition.lettersAlreadyUsed}"
-            }
-            is GameCondition.Won -> showGameWon(gameCondition.sentenceReveal)
-        }
-    }
-
-    private fun showGameLost(wordToGuess: String) {
-        wordTextView.text = wordToGuess
-        gameLostTextView.visibility = View.VISIBLE
-        imageView.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.game7))
-        lettersLayout.visibility = View.GONE
-    }
-
-    private fun showGameWon(wordToGuess: String) {
-        wordTextView.text = wordToGuess
-        gameWonTextView.visibility = View.VISIBLE
-        lettersLayout.visibility = View.GONE
-    }
-
-    private fun startNewGame() {
-        gameLostTextView.visibility = View.GONE
-        gameWonTextView.visibility = View.GONE
-        val gameCondition = gameControl.startNewGame()
-        lettersLayout.visibility = View.VISIBLE
-        lettersLayout.children.forEach { letterView ->
-            letterView.visibility = View.VISIBLE
-        }
-        updateUI(gameCondition)
-    }
-}
-
-    }
-
-
-
-
 }
